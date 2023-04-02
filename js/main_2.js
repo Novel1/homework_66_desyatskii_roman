6e2481d9941c79376cc456f0f882d2032da6c5a5
Roman Desyatskii <roman.desyatskii30@mail.ru>
2023-04-02 14:07:52 +0600

    задание 2, свойство style, перекрашивание текста

---
+++ js/main_2.js
@@ -0,0 +1,13 @@
+let pageDescription = document.getElementsByClassName('element')[0]
+let pageDescription_2 = document.getElementsByClassName('element')[1]
+let pageDescription_3 = document.getElementsByClassName('element')[2]
+pageDescription.style.color = 'red'
+pageDescription_2.style.color = 'red'
+pageDescription_3.style.color = 'red'
+
+let pageDescription_4 = document.getElementsByClassName('element')[3]
+let pageDescription_5 = document.getElementsByClassName('element')[4]
+let pageDescription_6 = document.getElementsByClassName('element')[5]
+pageDescription_4.style.color = 'green'
+pageDescription_5.style.color = 'green'
+pageDescription_6.style.color = 'green'
