0cf95660baedccf1e6e7c18c826493da731fae2d
Roman Desyatskii <roman.desyatskii30@mail.ru>
2023-04-02 14:05:39 +0600

    задание 1, обращение к тэгам, и вывод в консоль

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const divs3 = document.getElementById('string-3')
+console.log(divs3)
+const divs5 = document.getElementById('string-5')
+console.log(divs5)
+const divs2 = document.getElementById('string-2')
+console.log(divs2)
+const divs6 = document.getElementById('string-6')
+console.log(divs6)
+const divs4 = document.getElementById('string-4')
+console.log(divs4)
+const divs1 = document.getElementById('string-1')
+console.log(divs1)
+
+
